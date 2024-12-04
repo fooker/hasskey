1bb6f9d5b4e099b8adeb19fdc390fa7a207c6e6c
Dustin Frisch <fooker@lab.sh>
2024-12-04 20:35:30 +0100

    Upadte deps

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Sound routing daemon";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
     crane = {
       url = "github:ipetkov/crane";
